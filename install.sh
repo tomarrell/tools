4662c39a3be22b18c4f1d56db058a1f79a5ba10f
Tom Arrell <tom.arrell@gmail.com>
2019-08-03 14:05:06 +0200

    Added things

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@
 # Brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 
+# Install Zsh
+brew install zsh zsh-completions;
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
+source ~/.zshrc;
+
 # GUI software
 brew cask install kitty;
 brew cask install spectacle;
@@ -10,14 +17,22 @@ brew cask install slack;
 brew cask install firefox;
 brew cask install bitwarden;
 brew cask install spotify;
+brew cask install docker;
 
 # CLI tools
 brew install neovim;
 brew install python2;
 brew install python3;
 brew install go;
+brew install rustup;
 brew install tig;
+brew install fzf;
+brew install ripgrep;
+brew install tree;
 
 # Speed up key repeat, max is too slow
-defaults write -g InitialKeyRepeat -int 10
-defaults write -g KeyRepeat -int 1
+defaults write -g InitialKeyRepeat -int 10;
+defaults write -g KeyRepeat -int 1;
+
+# Install Xcode command line tools
+xcode-select --install;
